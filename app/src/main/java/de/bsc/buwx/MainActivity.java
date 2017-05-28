dec3bb708644c7a3afbb304bb56c989124fb0cb2
micha <buchfink@web.de>
2017-05-28 21:59:00 +0200

    Skip fullscreen activity

--- app/src/main/java/de/bsc/buwx/MainActivity.java
+++ app/src/main/java/de/bsc/buwx/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -25,25 +26,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         if (Wx.DEV) Log.d(LOG_TAG, "onCreate");
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        final WebView myWebView = (WebView)findViewById(R.id.fullscreen_content);
+        final WebView webView = (WebView)findViewById(R.id.webview_content);
 
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-//            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-//                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
-        myWebView.loadUrl(Wx.WEB_URL);
-        WebSettings webSettings = myWebView.getSettings();
+        webView.loadUrl(Wx.WEB_URL);
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        myWebView.setWebViewClient(new WebViewClient() {
+        webView.setWebViewClient(new WebViewClient() {
             @SuppressWarnings("deprecation")
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -54,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        myWebView.setOnKeyListener(new View.OnKeyListener()
+        webView.setOnKeyListener(new View.OnKeyListener()
         {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event)
@@ -79,19 +71,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        myWebView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-              | View.SYSTEM_UI_FLAG_FULLSCREEN
-              | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
         swipeLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_container);
         swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 swipeLayout.setRefreshing(true);
-                myWebView.reload();
+                webView.reload();
                 swipeLayout.setRefreshing(false);
             }
         });
@@ -103,12 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (Wx.DEV) Log.d(LOG_TAG, "onRestart");
 
-        WebView myWebView = (WebView)findViewById(R.id.fullscreen_content);
-        myWebView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        WebView webView = (WebView)findViewById(R.id.webview_content);
+        webView.reload();
     }
 }
